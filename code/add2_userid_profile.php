b0fdbd0596936408106bb1f7de879b453ae5efc4
megatshidiq <30791471+megatshidiq@users.noreply.github.com>
2017-08-09 19:00:20 +0800

    Update add2_userid_profile.php

--- code/add2_userid_profile.php
+++ code/add2_userid_profile.php
@@ -29,7 +29,7 @@ elseif($_GET['staffid']!='')
 {
 echo "masuk weehh";
      $staffnum=$_GET['staffid'];
-    include '/api_reader/getStaffProfile.php';
+    include 'api_reader/getStaffProfile.php';
     
     
 
