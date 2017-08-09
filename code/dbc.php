e04f8f461fcaa5bfe68189e0c80d508e5e9fe20e
megatshidiq <30791471+megatshidiq@users.noreply.github.com>
2017-08-09 15:46:08 +0800

    Update dbc.php

--- code/dbc.php
+++ code/dbc.php
@@ -1,5 +1,5 @@
 <?php
 $dbname = 'sa_review';
-$link = mysql_connect("localhost","root","adslprod") or die("Couldn't make connection.");
+$link = mysql_connect("172.30.109.133","idsa","adslprod") or die("Couldn't make connection.");
 $db = mysql_select_db($dbname, $link) or die("Couldn't select database");
 ?>
