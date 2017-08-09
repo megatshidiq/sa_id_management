377fd3aa933b282bfad41e1fa4a00da9f820f12b
megatshidiq <30791471+megatshidiq@users.noreply.github.com>
2017-08-09 19:12:14 +0800

    Update del_userid_profile.php

--- code/del_userid_profile.php
+++ code/del_userid_profile.php
@@ -4,7 +4,28 @@ $id = $_GET['sysid'];
 $sys_nameid = $_GET['sys_nameid'];
 
 $sql = "delete from user_detail where sysid='".$id."'";
-mysql_query($sql);
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+if ($conn->query($sql) === TRUE) {
+   // echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+
+
+
 echo  "<meta http-equiv='refresh' content='0; url=show_detail_review.php?pg=3&sysid=".$sys_nameid."' />";
 die();
  ?>
