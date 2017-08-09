4102ff457aa70a2345ed8dbd04c3f11eac4a853a
megatshidiq <30791471+megatshidiq@users.noreply.github.com>
2017-08-09 19:28:21 +0800

    Update edit_userid_profile.php

--- code/edit_userid_profile.php
+++ code/edit_userid_profile.php
@@ -50,7 +50,23 @@ $conn->close();
     $sql="update user_detail set userid='".$userid."',staff_id='".$staff_id."',fullname='".$fullname."',email='".$email."',Designation='".$Designation."',Remarks='".$Remarks."',privilege='".$privilege."' where  staff_id='".$staff_id."' ";
 
 
-     mysql_query($sql);
+  // Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+if (mysqli_query($conn, $sql)) {
+  //  echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+	 
     echo  "<meta http-equiv='refresh' content='0; url=edit_userid_profile.php?pg=3&sysid=".$sysid."' />";
   die();
  }
