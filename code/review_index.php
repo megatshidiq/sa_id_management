200795177683ebae314851d5c9da6e1ea8a94405
megatshidiq <30791471+megatshidiq@users.noreply.github.com>
2017-08-10 09:00:55 +0800

    Update review_index.php

--- code/review_index.php
+++ code/review_index.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+?>
+<?php
 require 'dbc.php';
 ?>
 <!doctype html>
@@ -42,7 +45,7 @@ require 'dbc.php';
 			<div class="logo">
     <a href="#" class="simple-text">
                   SYSTEM ADMINISTRATOR ANM
-				</a>
+				</a><?php echo $_SESSION['fullname'] ?>
 			</div>
 
 
